7e0632efa0400980d697a2571c2272e95b1fd25e
siyad <siyad@outlook.com>
2025-06-21 13:23:22 +0100

    Issues fixed for connecting database created with docker compose

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddCarter(new DependencyContextAssemblyCatalog(typeof(Program).Assembly));
 builder.Services.AddMediatR(config => { config.RegisterServicesFromAssemblies(typeof(Program).Assembly); });
-builder.Services.AddMarten(options => { options.Connection(builder.Configuration.GetConnectionString("DefaultConnection")); })
+builder.Services.AddMarten(options => { options.Connection(builder.Configuration.GetConnectionString("DefaultConnection")!); })
     .UseLightweightSessions();
 
 var app = builder.Build();
